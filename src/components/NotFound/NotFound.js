6638309416bcb2d10674bd59122c66e20704f781
unknown <imtiajahamed6161@gmail.com>
2021-10-01 10:51:37 -0700

    updated done

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <h3>The Page you are Looking for is not Available</h3>
+        </div>
+    );
+};
+
+export default NotFound;
---
+++ src/components/PlaceOrder/PlaceOrder.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import img from '../../images/giphy.gif'
+
+const PlaceOrder = () => {
+    return (
+        <div>
+            <img src={img} alt="" />
+            <h2>This is Place Order</h2>
+        </div>
+    );
+};
+
+export default PlaceOrder;
